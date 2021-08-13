8fcd9287aba9e18863fc0ac82a094fda572ea70d
Manson <mansonlautaro@gmail.com>
2021-08-13 18:42:45 -0300

    partial Update

--- Class11/Practice/src/routes/routes.js
+++ Class11/Practice/src/routes/routes.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.render('main');
-})
+const validation = (percent, minimum, maximum) => {
 
+      if(percent < minimum || percent > maximum || percent === '' || minimum > maximum || minimum === '' || maximum === ''){
+          return {error: true, message: `Please, insert the values correctly...`}
+      }else{
+          return {error: false, message: `Values submitted, look at the bar...`}
+      }
+}
+
+router.get('/', (req,res) => {
+    res.render('main', {errorExists: false, messageExists: false,form: true});
+});
+router.get('/data', (req,res) => {
+    const {percent, minimum, maximum} = req.query;
+    console.log(req.query)
+    const result = validation(+percent,+minimum,+maximum);
+    result.error ? res.render('main', {errorExists: true, messageExists: false, form: true, message: result.message}) :
+    res.render('main', {errorExists: false, messageExists: true, form: false, min: minimum, max: maximum, percent: percent})
+});
 export default router
