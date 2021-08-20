c117df7db1949da390a12ab4c4c5227a4cc62ce0
Manson <mansonlautaro@gmail.com>
2021-08-20 16:51:02 -0300

    final update

--- Class12/Entregable/src/public/index.js
+++ Class12/Entregable/src/public/index.js
@@ -1,14 +1,70 @@
-/*         CLEANING THE FORM INPUTS       */
-const socket = io();
+const socket = io.connect();
 const button = document.getElementById('button');
 const thumbnail = document.getElementById('thumbnail');
 const title = document.getElementById('title');
 const price = document.getElementById('price');
 
+const container = $("#table-container")[0];
+
+/* ---------------------------- ADDING PRODUCTS TO THE TABLE --------------------- */
+
+
+const addingTable = (body,data) => {
+    let inner = ''
+    data.map(product => {
+        inner+=`
+        <tr>
+          <th scope="row">${product.id}</th>
+          <td>${product.title}</td>
+          <td>${product.price}</td>
+          <td><img src="${product.thumbnail}" class="w-100 h-100" /></td>
+        </tr>
+        `;
+    });
+    body.innerHTML= inner;
+};
+
+/* ------------------------ SUBMIT BUTTON LISTENER (UPDATES AND CLEANING INPUTS) ---------------- */
+
 button.addEventListener('click', () => {
     title.innerHTML = '';
     thumbnail.innerHTML = '';
     price.innerHTML = '';
-    
-})   
+});   
+
+/* ------------------------------- LISTENING TO FIRST CONNECTIONS AND DISPLAYING THE PRODUCTS --------- */
+
+
+socket.on('show', data => {
+  if(typeof data === 'string' && $("#alert")[0] === undefined){
+    container.insertAdjacentHTML('afterbegin',`<div class="alert alert-danger alert-dismissible fade show mt-2" role="alert" id="alert">
+    <span>${data}
+      <br />Add products in the form above...
+      </span>
+  </div>`);
+  }else if($("#table")[0] === undefined){
+    container.insertAdjacentHTML('afterbegin',`
+        <table class="table table-stripped table-hover" id="table" ></table>`
+    ); 
+    const table = container.children[0];
+    table.insertAdjacentHTML('afterbegin',
+        `<thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Image</th>
+        </tr>
+      </thead>`
+    );
+    table.insertAdjacentHTML('beforeend',`<tbody></tbody>`);
+    const body = table.children[1];
+    addingTable(body,data);
+  }else{
+    if($("#alert")[0])$("#alert")[0].remove();
+    const body = container.children[0].children[1]; // <TBODY>
+    addingTable(body,data)
+  }
+});
+
 
--- Class12/Entregable/src/server.js
+++ Class12/Entregable/src/server.js
@@ -1,78 +1,110 @@
-import express from 'express';
-import path from 'path';
-import router from './routes/routes.js';
-import handlebars from 'express-handlebars';
-
+import express from "express";
+import path from "path";
+import productClass from './class'
+import handlebars from "express-handlebars";
 
 /*-------------  INITIALIZING SERVER & APP  -----------------*/
 const PORT = 8080;
 const APP = express();
-const publicPath = path.resolve(__dirname, './public')
+const publicPath = path.resolve(__dirname, "./public");
 
 APP.use(express.json());
 APP.use(express.urlencoded({ extended: true }));
 
 /*-------------  SERVER LISTENING PORT & ERRORS  -----------*/
-const http = require('http').Server(APP);
+const http = require("http").Server(APP);
 
 http.listen(PORT, () => {
-
-    console.log(`Hi! This server is hosted at PORT: ${http.address().port}`);
+  console.log(`Hi! This server is hosted at PORT: ${http.address().port}`);
 });
-http.on('error', error => {
-    console.log(`Error: ${error}`)
+http.on("error", (error) => {
+  console.log(`Error: ${error}`);
 });
 
-APP.use('/api', router);
 APP.use(express.static(publicPath));
 
 /*--------------- HBS TEMPLATE ENGINE CONFIGURATION  --------*/
 
-APP.engine('hbs', handlebars({
-    extname: '.hbs',
-    defaultLayout: 'index.hbs',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials',
-}));
+APP.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
 
-APP.set('views', './src/views');
-APP.set('view engine', 'hbs');
+APP.set("views", "./src/views");
+APP.set("view engine", "hbs");
 
-APP.get('/products/view', (req, res) => {
-    res.render('form', {
-        errorExist: false, messageExist: false,
-        action: "http://localhost:8080/api/products/save",
-        method: "post"
-    });
-})
+
+/* ----------------- TESTING OBJECTS ----------------------- */
+const test = new productClass();
+
+
+test.addUpdateProduct(
+  "Roast Beef",
+  8.99,
+  "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/327DBA15-0C31-4CC6-ABCA-2FCE38AF66CD/Derivates/edb1c351-ed50-4560-a2d1-bf3e0be1a04d.jpg"
+);
+test.addUpdateProduct(
+  "Milk",
+  1.29,
+  "https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/raw-milk-1-e1563894986431.jpg"
+);
+test.addUpdateProduct(
+  "Beans",
+  0.99,
+  "https://static.independent.co.uk/2021/01/04/09/iStock-969582980.jpg?width=982&height=726&auto=webp&quality=75"
+);
+
+
+/* --------------  VALIDATING EMPTY PROPERTIES  ----------------- */
+
+const saveUpdateVal = async (title, price, thumbnail, id = null) => {
+    if (title !== "" && price !== "" && thumbnail !== "") {
+      return await test.addUpdateProduct(title, price, thumbnail, id);
+    } else {
+      return new Error(`Please set the product properties correctly...`);
+    }
+  };
+
+
+
 
 /* ------------------ SOCKET IMPLEMENTATION ----------------------- */
 
-const io = require('socket.io')(http);
-const test = require('./class.js');
+const io = require("socket.io")(http);
 
-
-io.on('connection', socket => {
-    console.log('New client connected!');
-    socket.emit()
+APP.get("/socket/form", (req, res) => {
+  res.render("form", { errorExists: false, messageExists: false });
 });
 
-APP.get('/socket/form', (req,res) => {
-    const result = await test.getProducts();
-    result.length > 0
-    ? res.render("form", { 
-        errorExists: false, 
-        productsExist: true,
-        items: true,
-        products: result,
+APP.post('/socket/form', async (req,res) => {
+  const { title, price, thumbnail } = req.body;
+  const result = await saveUpdateVal(title, price, thumbnail);
+  const err = /Error:/gi;
+  err.test(result)     // IF RESULTS WITH AN ERROR, IT WILL RENDER 
+    ? res.render("form", { // AN ERROR ALERT OR A SUCCESSFUL ALERT
+        message: `${result}`,
+        errorExists: true,
+        messageExists: false,
       })
     : res.render("form", {
-        errorExists: true,
-        productsExist: false,
-        message: result.error,
+        message: `${result}`,
+        errorExists: false,
+        messageExists: true,
       });
-    
+})
+
+io.on("connection", async (socket) => {
+  console.log("New client connected!");
+  let result = await test.getProducts();
+  if (result.length > 0) {
+    io.sockets.emit("show", result);
+  } else {
+    io.sockets.emit("show", result.error);
+  }
+
 });
-
-
-  
