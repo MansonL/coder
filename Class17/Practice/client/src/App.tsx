2cfab71b5cd5c31484a09523732a8b35dcf3363d
Manson <mansonlautaro@gmail.com>
2021-09-07 18:43:39 -0300

    partialUpdate

--- Class17/Practice/client/src/App.tsx
+++ Class17/Practice/client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
-import Browser
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App(): JSX.Element {
   return(
       <Router>
-
+      <div className="container-fluid d-flex align-items-center vh-100">
+          <Switch>
+              <Route path="/">
+               <Home />
+              </Route>
+              <Route path="/chat">
+                  <Layout />
+              </Route>
+          </Switch>
+      </div>
       </Router>
   )
 }
